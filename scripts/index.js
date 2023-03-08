781c11c1314da7a8466a4904cba51e6d42a2ea47
Kot <katkova.alla@gmail.com>
2023-03-08 18:27:31 +0300

    feat: js динамичное добавление карточек из массива , открытие и закрытие поп ап новое место, добавление картинки и названия в начало сетки

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,17 @@
 //открытие и закрытие поп ап
 const profileButtonEdit = document.querySelector(".profile__button-edit");
+const profileButtonAdd = document.querySelector(".profile__button-add");
+console.log('добавления кнопка: ', profileButtonAdd);
+
+
 //console.log('редакт кнопка: ', profileButtonEdit);
 const popup = document.querySelector(".popup");
-
-function openPopup() {
-  popup.classList.remove("popup_hidden")
-  // Информация из профиля пользователя загружается в соответствующие поля
-  nameInput.value = profileName.textContent;
-  statusInput.value = profileStatus.textContent;
-}
+const popupAdd = document.querySelector(".popup-add")
 
 const popupCloseButton = popup.querySelector(".popup__close-button");
-//console.log('закрытия кнопка: ', popupCloseButton);
-
-function closePopup() {
-  popup.classList.add("popup_hidden")
-}
-profileButtonEdit.addEventListener("click", openPopup);
-popupCloseButton.addEventListener("click", closePopup);
-
+const popupCloseButtonAdd = popupAdd.querySelector(".popup__close-button-add");
+console.log('закрытия кнопка: ', popupCloseButtonAdd);
+console.log('закр кнопка: ', popupCloseButton);
 
 /*popup__save-button  кнопка сохранения*/
 let nameInput = popup.querySelector(".popup__input_type_name");
@@ -31,15 +24,75 @@ let profileName = document.querySelector(".profile__name");
 let profileStatus = document.querySelector(".profile__status");
 //console.log("профиль статус " , profileStatus);
 
+
+let placeNameInput = popupAdd.querySelector(".popup__input_type_place_name");
+let  photoLinkInput = popupAdd.querySelector(".popup__input_type_photo_link");
+let popupFormAdd = popupAdd.querySelector(".popup__form_add")
+//const popupSaveButtonAdd = popupAdd.querySelector(".popup__save-button_add");
+
+
+function openPopup() {
+  popup.classList.remove("popup_hidden")
+  // Информация из профиля пользователя загружается в соответствующие поля
+  nameInput.value = profileName.textContent;
+  statusInput.value = profileStatus.textContent;
+}
+
+
+function openPopupAdd() {
+  popupAdd.classList.remove("popup_hidden")
+}
+//function closePopupAdd() {
+  //popupAdd.classList.add("popup_hidden")
+//}
+
+
+//function closePopup() {
+  //popup.classList.add("popup_hidden")
+//}
+
+function closePopupUniversal(element) {
+element.classList.add("popup_hidden");
+}
+
+profileButtonEdit.addEventListener("click", openPopup);
+popupCloseButton.addEventListener("click", function() {
+  closePopupUniversal(popup)
+});
+profileButtonAdd.addEventListener("click", openPopupAdd);
+popupCloseButtonAdd.addEventListener("click", function() {
+  closePopupUniversal(popupAdd)
+});
+
+
 function submitForm(event) {
   event.preventDefault();
   //console.log(event);
   profileName.textContent = nameInput.value;
   profileStatus.textContent = statusInput.value;
-  closePopup()
+  closePopupUniversal(popup)
+}
+
+
+function submitFormAdd(event) {
+  event.preventDefault();
+
+  const newElement = document.querySelector('#elementTemplate').content.cloneNode(true);
+  const elementPhoto = newElement.querySelector('.element__photo');
+  elementPhoto.setAttribute('src', photoLinkInput.value);
+  const elementCaption = newElement.querySelector('.element__caption');
+  elementCaption.textContent = placeNameInput.value;
+  elements.prepend(newElement);
+
+  placeNameInput.value = "";
+  photoLinkInput.value = "";
+  closePopupUniversal(popupAdd)
 }
 
 popupForm.addEventListener("submit", submitForm);
+popupFormAdd.addEventListener("submit", submitFormAdd);
+
+
 
 //При загрузке на странице 6 карточек, которые добавит JavaScript
 const initialCards = [
@@ -68,3 +121,17 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
+
+const elements = document.querySelector('.elements')
+initialCards.forEach(function(element) {
+  const newElement = document.querySelector('#elementTemplate').content.cloneNode(true);
+  const elementPhoto = newElement.querySelector('.element__photo');
+  elementPhoto.setAttribute('src', element.link);
+  console.log(element);
+  const elementCaption = newElement.querySelector('.element__caption');
+  elementCaption.textContent = element.name;
+  elements.append(newElement);
+}
+)
+
+
