0a21d70dd0cd0ef6a7efb24882e3b66d56bedac2
Kot <katkova.alla@gmail.com>
2023-03-10 17:37:16 +0300

    feat: js лайк становиться черны при нажатии

--- scripts/index.js
+++ scripts/index.js
@@ -26,7 +26,7 @@ let profileStatus = document.querySelector(".profile__status");
 
 
 let placeNameInput = popupAdd.querySelector(".popup__input_type_place_name");
-let  photoLinkInput = popupAdd.querySelector(".popup__input_type_photo_link");
+let photoLinkInput = popupAdd.querySelector(".popup__input_type_photo_link");
 let popupFormAdd = popupAdd.querySelector(".popup__form_add")
 //const popupSaveButtonAdd = popupAdd.querySelector(".popup__save-button_add");
 
@@ -43,24 +43,24 @@ function openPopupAdd() {
   popupAdd.classList.remove("popup_hidden")
 }
 //function closePopupAdd() {
-  //popupAdd.classList.add("popup_hidden")
+//popupAdd.classList.add("popup_hidden")
 //}
 
 
 //function closePopup() {
-  //popup.classList.add("popup_hidden")
+//popup.classList.add("popup_hidden")
 //}
 
 function closePopupUniversal(element) {
-element.classList.add("popup_hidden");
+  element.classList.add("popup_hidden");
 }
 
 profileButtonEdit.addEventListener("click", openPopup);
-popupCloseButton.addEventListener("click", function() {
+popupCloseButton.addEventListener("click", function () {
   closePopupUniversal(popup)
 });
 profileButtonAdd.addEventListener("click", openPopupAdd);
-popupCloseButtonAdd.addEventListener("click", function() {
+popupCloseButtonAdd.addEventListener("click", function () {
   closePopupUniversal(popupAdd)
 });
 
@@ -82,69 +82,80 @@ function submitFormAdd(event) {
   elementPhoto.setAttribute('src', photoLinkInput.value);
   const elementCaption = newElement.querySelector('.element__caption');
   elementCaption.textContent = placeNameInput.value;
-  
+
   const deleteButton = newElement.querySelector('.element__delete-button');
   deleteButton.addEventListener('click', handleDeleteButtonClick);
 
-  elements.prepend(newElement);
+  const likeButton = newElement.querySelector('.element__like-button');
+  likeButton.addEventListener('click', function (event) {
+    event.target.classList.toggle('element__like-button_active');
+  });
 
-  placeNameInput.value = "";
-  photoLinkInput.value = "";
-  closePopupUniversal(popupAdd)
-}
+    elements.prepend(newElement);
+
+    placeNameInput.value = "";
+    photoLinkInput.value = "";
+    closePopupUniversal(popupAdd)
+  }
 
 popupForm.addEventListener("submit", submitForm);
-popupFormAdd.addEventListener("submit", submitFormAdd);
+  popupFormAdd.addEventListener("submit", submitFormAdd);
 
 
 
-//При загрузке на странице 6 карточек, которые добавит JavaScript
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  //При загрузке на странице 6 карточек, которые добавит JavaScript
+  const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+  const elements = document.querySelector('.elements')
+  initialCards.forEach(function (element) {
+    const newElement = document.querySelector('#elementTemplate').content.cloneNode(true);
+    const elementPhoto = newElement.querySelector('.element__photo');
+    elementPhoto.setAttribute('src', element.link);
+    console.log(element);
+    const elementCaption = newElement.querySelector('.element__caption');
+    elementCaption.textContent = element.name;
+
+    const deleteButton = newElement.querySelector('.element__delete-button');
+    deleteButton.addEventListener('click', handleDeleteButtonClick);
+
+    const likeButton = newElement.querySelector('.element__like-button');
+    likeButton.addEventListener('click', function (event) {
+      event.target.classList.toggle('element__like-button_active');
+    })
+
+    elements.append(newElement);
   }
-];
+  )
 
-const elements = document.querySelector('.elements')
-initialCards.forEach(function(element) {
-  const newElement = document.querySelector('#elementTemplate').content.cloneNode(true);
-  const elementPhoto = newElement.querySelector('.element__photo');
-  elementPhoto.setAttribute('src', element.link);
-  console.log(element);
-  const elementCaption = newElement.querySelector('.element__caption');
-  elementCaption.textContent = element.name;
-  const deleteButton = newElement.querySelector('.element__delete-button');
-  deleteButton.addEventListener('click', handleDeleteButtonClick);
+  function handleDeleteButtonClick(event) {
+    const button = event.target;
+    const elementToDelete = button.closest('.element');
+    elementToDelete.remove();
+  }
 
-
-  elements.append(newElement);
-}
-)
-
-function handleDeleteButtonClick(event) {
-const button = event.target;
-const elementToDelete = button.closest('.element');
-elementToDelete.remove();
-}
 
