80aa0970415e9114e1626dca41ad0fdb8e3209e4
Kot <katkova.alla@gmail.com>
2023-05-11 18:03:12 +0300

    feat:  Создала класс UserInfo

---
+++ scripts/UserInfo.js
@@ -0,0 +1,19 @@
+//import Popup from "./Popup.js";
+
+export default class UserInfo {
+  constructor(userInfoConfig) {
+    this._userName = document.querySelector(userInfoConfig.nameSelector);
+    this._userStatus = document.querySelector(userInfoConfig.statusSelector);
+  }
+
+  getUserInfo() {
+    return {profilename: this._userName.textContent, profilestatus: this._userStatus.textContent}
+  }
+
+
+  setUserInfo(newDataUser) {
+    this._userName.textContent = newDataUser.profilename
+    this._userStatus.textContent = newDataUser.profilestatus
+  }
+
+}
--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ import FormValidator from './FormValidator.js';
 import Section from './Section.js';
 import PopupWithImage from './PopupWithImage.js';
 import { initialCards } from '../utils/constants.js'
+import UserInfo from './UserInfo.js';
 
 // селекторы для валидации
 const validationConfig = {
@@ -12,59 +13,46 @@ const validationConfig = {
   inactiveButtonClass: 'popup__save-button_invalid',
   inputErrorClass: 'popup__input_type_error'
 }
+
+const userInfoConfig = {
+  nameSelector: ".popup__input_type_name",
+  statusSelector: ".popup__input_type_status"
+}
+
 // все формы
 const forms = Array.from(document.querySelectorAll(validationConfig.formSelector))
-
 const profileButtonEdit = document.querySelector(".profile__button-edit");
 const profileButtonAdd = document.querySelector(".profile__button-add");
-
 const popupProfileEdit = document.querySelector(".popup-edit");
-
 const popupAdd = document.querySelector(".popup-add")
-
 const popupCloseButtonProfileEdit = popupProfileEdit.querySelector(".popup__close-button");
 const popupCloseButtonAdd = popupAdd.querySelector(".popup__close-button-add");
-
-const nameInput = popupProfileEdit.querySelector(".popup__input_type_name");
-const statusInput = popupProfileEdit.querySelector(".popup__input_type_status");
-
+// const nameInput = popupProfileEdit.querySelector(".popup__input_type_name");
+// const statusInput = popupProfileEdit.querySelector(".popup__input_type_status");
 const popupFormProfileEdit = popupProfileEdit.querySelector(".popup__form");
 const profileName = document.querySelector(".profile__name");
-
 const profileStatus = document.querySelector(".profile__status");
-
 const placeNameInput = popupAdd.querySelector(".popup__input_type_place-name");
 const photoLinkInput = popupAdd.querySelector(".popup__input_type_photo-link");
 const popupFormAdd = popupAdd.querySelector(".popup__form_add")
-
-const popupZoom = document.querySelector('.popup-zoom')
 const elements = document.querySelector('.elements')
-
-const popupZoomImages = popupZoom.querySelector('.popup__images-zoom');
-const popupZoomCaption = popupZoom.querySelector('.popup__caption-zoom');
-
-const popupList = document.querySelectorAll('.popup')
-
-//const cardTemplate = document.querySelector('#elementTemplate').content.querySelector('.element')
 
 
 
 const elementsSelector = '.elements'
-const popupProfileSelector = ".popup-edit"
 const popupZoomSelector = '.popup-zoom';
 
-
-
-
-//const profilePopup = new Popup(popupProfileSelector);
 const popupImageZoom = new PopupWithImage(popupZoomSelector)
 popupImageZoom.setEventListeners();
 
+const profilePopup = new UserInfo(userInfoConfig);
 
-const section = new Section ({
+
+
+const section = new Section({
   items: initialCards, //мой массив карточек
   renderer: (cardData) => {
-    const card = new Card (cardData, '#elementTemplate', popupImageZoom.open);//в рендере создаем карточку (это колбэк фунция для создания карточки)
+    const card = new Card(cardData, '#elementTemplate', popupImageZoom.open);//в рендере создаем карточку (это колбэк фунция для создания карточки)
     return card.generateCard(); //и возвращаем методом generate card дом элемент карточки со всеми слушателями
   }
 }, elementsSelector) //создала константу-селектор для контейнера карточек
@@ -87,10 +75,10 @@ section.render()
 // });
 
 const validators = {};
-forms.forEach((formElement) => { 
-  const validator = new FormValidator(validationConfig, formElement); 
+forms.forEach((formElement) => {
+  const validator = new FormValidator(validationConfig, formElement);
   validator.enableValidation();
-  validators[formElement.getAttribute('name')] = validator; 
+  validators[formElement.getAttribute('name')] = validator;
 });
 
 
@@ -104,7 +92,7 @@ function openPopupProfileEdit() {
 }
 // либо удалю, либо запихну в наследника PopupWithForm
 function openPopupAdd() {
- // openPopupUniversal(popupAdd);
+  // openPopupUniversal(popupAdd);
 }
 
 //запихну в popup наследника UserInfo
@@ -136,7 +124,7 @@ function submitFormAdd(event) {
     link: photoLinkInput.value
   };
 
-//запихну в popup наследника PopupWithForm
+  //запихну в popup наследника PopupWithForm
   const newCard = createCard(cardData);
   elements.prepend(newCard);
   //closePopupUniversal(popupAdd);
